f2556d363468f927d6f8c031a5f8cfcc17604912
Jon Brisbin <jon@moderne.io>
2021-09-25 11:46:55 -0500

    Initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,158 @@
+import nl.javadude.gradle.plugins.license.LicenseExtension
+import java.util.*
+
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
+    id("nebula.release") version "15.3.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+
+    id("com.github.hierynomus.license") version "0.16.1"
+
+    id("nebula.maven-publish") version "17.3.2"
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.3.0"
+
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+    id("nebula.maven-apache-license") version "17.3.2"
+}
+
+apply(plugin = "nebula.publish-verification")
+
+configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
+    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
+}
+
+group = "org.openrewrite.polyglot"
+description = "Enforce logging best practices and migrate between logging frameworks. Automatically."
+
+repositories {
+    if (!project.hasProperty("releasing")) {
+        mavenLocal()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+    }
+    mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+signing {
+    setRequired({
+        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
+    })
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["nebula"])
+}
+
+configurations.all {
+    resolutionStrategy {
+        cacheChangingModulesFor(0, TimeUnit.SECONDS)
+        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
+    }
+}
+
+val rewriteVersion = if (project.hasProperty("releasing")) {
+    "latest.release"
+} else {
+    "latest.integration"
+}
+
+
+tasks.withType<Javadoc> {
+    exclude("**/RewriteAnnotationProcessor**")
+}
+
+dependencies {
+    implementation("org.openrewrite:rewrite-core:latest.integration")
+    implementation("org.openrewrite:rewrite-java-11:latest.integration")
+
+    testImplementation("org.jooq:joor:latest.release")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.openrewrite:rewrite-test:latest.release")
+    testImplementation("org.assertj:assertj-core:latest.release")
+
+    testImplementation("com.google.auto.service:auto-service:latest.release")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.0.13")
+}
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+
+    options.isFork = true
+    options.forkOptions.executable = "javac"
+    options.compilerArgs.addAll(listOf("--release", "8"))
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
+
+
+configure<nebula.plugin.contacts.ContactsExtension> {
+    val j = nebula.plugin.contacts.Contact("team@moderne.io")
+    j.moniker("Team Moderne")
+
+    people["team@moderne.io"] = j
+}
+
+configure<LicenseExtension> {
+    ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
+    skipExistingHeaders = true
+    header = project.rootProject.file("gradle/licenseHeader.txt")
+    mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
+    exclude("src/main/resources/*.java")
+    strictCheck = true
+}
+
+configure<PublishingExtension> {
+    publications {
+        named("nebula", MavenPublication::class.java) {
+            suppressPomMetadataWarningsFor("runtimeElements")
+        }
+    }
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
+
+    options.compilerArgs.clear() // remove `--release 8` set in root gradle build
+    options.compilerArgs.addAll(listOf(
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED"
+    ))
+}
