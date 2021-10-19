97877007417d138dfcdbd9c6320708310e7c1be1
Aaron Gershman <aegershman@gmail.com>
2021-10-19 16:49:47 -0500

    Gradle enterprise for openrewrite

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,27 @@ pluginManagement {
 }
 
 rootProject.name = "rewrite-polyglot"
+
+plugins {
+    id("com.gradle.enterprise") version "3.7"
+}
+
+gradleEnterprise {
+    server = "https://ge.openrewrite.org/"
+
+    buildCache {
+        local {
+            isEnabled = true
+        }
+
+        remote(HttpBuildCache::class) {
+            isPush = true
+            url = uri("https://ge.openrewrite.org/cache/")
+        }
+
+    }
+
+    buildScan {
+        publishAlways()
+    }
+}
