15bea83c1e93e401f20811c377c2d0d31dfa1af8
Aaron Gershman <aegershman@gmail.com>
2021-10-29 15:22:53 -0500

    gradle buildScan publishIfAuthenticated

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,12 +24,15 @@ gradleEnterprise {
     }
 
     buildScan {
-        publishAlways()
-        isUploadInBackground = !isCiServer
-
         capture {
             isTaskInputFiles = true
         }
+
+        isUploadInBackground = !isCiServer
+
+        publishAlways()
+        this as com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+        publishIfAuthenticated()
     }
 
 }
