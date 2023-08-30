14bf50d41fa4acca53957c1e93254a6a86140dd8
Jonathan Schneider <jkschneider@gmail.com>
2023-08-30 13:55:17 -0400

    SourceFileStream

---
+++ src/main/java/org/openrewrite/polyglot/SourceFileStream.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.polyglot;
+
+import lombok.experimental.Delegate;
+import org.openrewrite.SourceFile;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class SourceFileStream implements Stream<SourceFile> {
+    @Delegate
+    private final Stream<SourceFile> delegate;
+
+    private final String group;
+    private final Consumer<String> peekGroup;
+    private final int size;
+
+    public static SourceFileStream build(String group, Consumer<String> peekGroup) {
+        Set<String> seenGroup = new HashSet<>();
+        return new SourceFileStream(Stream.empty(), 0, group, g -> {
+            if (seenGroup.add(g)) {
+                peekGroup.accept(g);
+            }
+        });
+    }
+
+    private SourceFileStream(Stream<SourceFile> delegate, int size, String group, Consumer<String> peekGroup) {
+        this.size = size;
+        this.delegate = delegate.peek(sf -> peekGroup.accept(group));
+        this.group = group;
+        this.peekGroup = peekGroup;
+    }
+
+    public SourceFileStream concat(Stream<SourceFile> sourceFileStream, int size) {
+        return new SourceFileStream(Stream.concat(delegate, sourceFileStream),
+                size + this.size, group, peekGroup);
+    }
+
+    public int size() {
+        return size;
+    }
+}
---
+++ src/test/java/org/openrewrite/polyglot/SourceFileStreamTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.polyglot;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.SourceFile;
+import org.openrewrite.text.PlainTextParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SourceFileStreamTest {
+
+    @Test
+    void singleProject() {
+        Stream<SourceFile> p1Sources = parse("a", "b", "c");
+        Stream<SourceFile> p1Resources = parse("d", "e", "f");
+        List<String> groups = new ArrayList<>();
+
+        assertThat(SourceFileStream.build("p1", groups::add)
+          .concat(p1Sources, 3)
+          .concat(p1Resources, 3)
+          .reduce(0, (n, sourceFile) -> n + 1, Integer::sum)).isEqualTo(6);
+        assertThat(groups).containsExactly("p1");
+    }
+
+    @Test
+    void multipleProjects() {
+        Stream<SourceFile> p1Sources = parse("a", "b", "c");
+        Stream<SourceFile> p2Sources = parse("d", "e", "f");
+        List<String> groups = new ArrayList<>();
+
+        assertThat(
+          Stream.concat(
+              SourceFileStream.build("p1", groups::add).concat(p1Sources, 3),
+              SourceFileStream.build("p2", groups::add).concat(p2Sources, 3)
+            )
+            .reduce(0, (n, sourceFile) -> n + 1, Integer::sum)).isEqualTo(6);
+        assertThat(groups).containsExactly("p1", "p2");
+    }
+
+    private static Stream<SourceFile> parse(String... files) {
+        return PlainTextParser.builder().build().parse(files);
+    }
+}
