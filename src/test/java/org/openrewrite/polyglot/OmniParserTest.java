7ecde851b98767ef92304d9e91c9d75464b970e9
Joan Viladrosa <joan@moderne.io>
2023-09-26 17:06:40 +0200

    Using jgit to list files and validate ignored paths

--- src/test/java/org/openrewrite/polyglot/OmniParserTest.java
+++ src/test/java/org/openrewrite/polyglot/OmniParserTest.java
@@ -16,7 +16,13 @@
 package org.openrewrite.polyglot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.openrewrite.shaded.jgit.api.Git;
+import org.openrewrite.shaded.jgit.util.FileUtils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -32,4 +38,48 @@ public class OmniParserTest {
         assertThat(parser.isExcluded(Paths.get("/Users/jon/Projects/github/quarkusio/gizmo/pom.xml"),
           Paths.get("/Users/jon/Projects/github/quarkusio/gizmo"))).isTrue();
     }
+
+    @Test
+    void acceptedPaths(@TempDir Path repo) throws IOException {
+        // Prepare repo
+        FileUtils.touch(repo.resolve("pom.xml"));
+        FileUtils.touch(repo.resolve("gitignored.xml"));
+        FileUtils.touch(repo.resolve("localexclude.xml"));
+        Files.writeString(repo.resolve(".gitignore"), "gitignored.xml");
+        FileUtils.touch(repo.resolve("file.xml"));
+        FileUtils.mkdirs(repo.resolve("folder").toFile());
+        FileUtils.touch(repo.resolve("folder/fileinfolder.xml"));
+        initGit(repo);
+        FileUtils.mkdirs(repo.resolve(".git/info").toFile());
+        Files.writeString(repo.resolve(".git/info/exclude"), "localexclude.xml");
+        FileUtils.touch(repo.resolve("newfile.xml"));
+        FileUtils.createSymLink(repo.resolve("symlink.xml").toFile(), "./newfile.xml");
+        // Repo prepared
+
+        OmniParser parser = OmniParser.builder(OmniParser.defaultResourceParsers())
+          .exclusions(List.of(Paths.get("pom.xml")))
+          .build();
+        List<Path> paths = parser.acceptedPaths(repo);
+        assertThat(paths).contains(
+          repo.resolve("file.xml"),
+          repo.resolve("newfile.xml"),
+          repo.resolve("folder/fileinfolder.xml")
+        ).doesNotContain(
+          repo.resolve("pom.xml"),
+          repo.resolve("gitignored.xml"),
+          repo.resolve("localexclude.xml"),
+          repo.resolve("symlink.xml")
+        );
+
+    }
+
+    void initGit(Path repositoryPath) {
+        try (Git git = Git.init().setDirectory(repositoryPath.toFile()).call()) {
+            git.add().addFilepattern(".").call();
+            git.commit().setSign(false).setMessage("init commit").call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
