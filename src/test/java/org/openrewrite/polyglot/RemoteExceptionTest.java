b897bd563223136df395f8730b8fe0886909cca1
Bryce Tompkins <bryce@moderne.io>
2024-09-11 14:58:58 -0400

    Add RemoteException#getPartialSuccess

--- src/test/java/org/openrewrite/polyglot/RemoteExceptionTest.java
+++ src/test/java/org/openrewrite/polyglot/RemoteExceptionTest.java
@@ -17,17 +17,62 @@ package org.openrewrite.polyglot;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Base64;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RemoteExceptionTest {
-    RemoteException remote = RemoteException.builder("This is a bad thing")
-      .cause(new RuntimeException("boom"), "org.openrewrite")
-      .fixSuggestions("Please fix")
-      .build();
 
     @Test
     void encodeDecode() {
+        RemoteException remote = RemoteException.builder("This is a bad thing")
+          .cause(new RuntimeException("boom"), "org.openrewrite")
+          .fixSuggestions("Please fix")
+          .partialSuccess(true)
+          .build();
+
         RemoteException decoded = RemoteException.decode(remote.encode());
         assertThat(decoded).isEqualTo(remote);
+    }
+
+    @Test
+    void encodeDecodeWithoutCause() {
+        RemoteException remote = RemoteException.builder("This is a bad thing")
+          .fixSuggestions("Please fix")
+          .build();
+
+        RemoteException decoded = RemoteException.decode(remote.encode());
+        assertThat(decoded).isEqualTo(remote);
+    }
+
+    @Test
+    void encodeDecodeWithoutSuggestions() {
+        RemoteException remote = RemoteException.builder("This is a bad thing")
+          .cause(new RuntimeException("boom"), "org.openrewrite")
+          .build();
+
+        RemoteException decoded = RemoteException.decode(remote.encode());
+        assertThat(decoded).isEqualTo(remote);
+    }
+
+    @Test
+    void decodeWithoutPartialSuccess() {
+        // To test deserializing exceptions serialized using legacy versions of rewrite-polyglot
+        Base64.Encoder base64 = Base64.getEncoder();
+        StringBuilder builder = new StringBuilder(256);
+        builder.append(base64.encodeToString("This is a bad thing".getBytes(UTF_8))).append('\n');
+        builder.append("null").append('\n');
+        builder.append("null");
+
+        RemoteException remote = RemoteException.builder("This is a bad thing")
+          .build();
+
+        RemoteException decoded = RemoteException.decode(builder.toString());
+        assertThat(decoded.getMessage()).isEqualTo(remote.getMessage());
+        assertThat(decoded.getCause()).isEqualTo(remote.getCause());
+        assertThat(decoded.getFixSuggestions()).isEqualTo(remote.getFixSuggestions());
+        assertThat(decoded.isPartialSuccess()).isEqualTo(remote.isPartialSuccess());
     }
 }
