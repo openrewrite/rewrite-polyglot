124336b970095dccd860b8e438f87919196b2f7f
Tim te Beek <tim@moderne.io>
2025-01-03 22:44:59 +0100

    Add missing import

--- src/main/java/org/openrewrite/polyglot/OmniParser.java
+++ src/main/java/org/openrewrite/polyglot/OmniParser.java
@@ -19,6 +19,7 @@ import org.openrewrite.jgit.treewalk.filter.PathFilter;
 import org.openrewrite.json.JsonParser;
 import org.openrewrite.properties.PropertiesParser;
 import org.openrewrite.protobuf.ProtoParser;
+import org.openrewrite.toml.TomlParser;
 import org.openrewrite.xml.XmlParser;
 import org.openrewrite.yaml.YamlParser;
 
