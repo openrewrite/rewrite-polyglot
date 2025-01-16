5cdcc26d127f9df9c0b09c325b85fda14d80fc65
Tim te Beek <tim@moderne.io>
2025-01-16 23:05:35 +0100

    Apply ASL (#43)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("org.openrewrite.build.recipe-library") version "latest.release"
-    id("org.openrewrite.build.moderne-proprietary-license") version "latest.release"
 }
 // Set as appropriate for your organization
 group = "org.openrewrite"
